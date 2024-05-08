9131fbaf94613f34444cb28b72b7bf2f46ffe280
Eeman Majumder <54275491+Eeman1113@users.noreply.github.com>
2024-05-08 09:03:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import anipy_cli
 import streamlit as st
-import time
 
 def main():
     st.markdown("<h1 style='text-align: center; color: #ff6961;'>Anius【アイヌス】🎬</h1>", unsafe_allow_html=True)
@@ -11,24 +10,17 @@ def main():
 
     if query_term:
         entry = anipy_cli.Entry()
-
-        # Show progress bar
-        progress_text = "Searching for anime..."
-        progress_bar = st.progress(0, text=progress_text)
-        for percent_complete in range(100):
-            time.sleep(0.01)
-            progress_bar.progress(percent_complete + 1, text=progress_text)
-
         query_class = anipy_cli.query(query_term, entry)
         links_and_names = query_class.get_links()
 
-        # Store search results in backend
+        # Store search results in backend with serial numbers
         search_results = []
-        for link, name in zip(links_and_names[0], links_and_names[1]):
-            search_results.append((name, f"https://gogoanime.gg{link}"))
+        for i, (link, name) in enumerate(zip(links_and_names[0], links_and_names[1]), start=1):
+            search_results.append((f"{i}. {name}", f"https://gogoanime.gg{link}"))
 
         # Step 2: Select an anime from the search results
         selected_anime = st.selectbox("Select an anime: 📽️", options=[name for name, _ in search_results])
+
         if selected_anime:
             selected_url = next(url for name, url in search_results if name == selected_anime)
             entry.category_url = selected_url
@@ -42,6 +34,7 @@ def main():
 
             # Step 4: Get the user's episode selection
             selected_episode = st.selectbox(f"Select an episode number: 📼 [1-{latest_episode}]", available_episodes)
+
             if selected_episode:
                 entry.ep = selected_episode
                 entry = ep_handler.gen_eplink()
@@ -54,11 +47,10 @@ def main():
                 # Output the embed link
                 st.success(f"Here You Go: 🎥 {entry.embed_url}")
 
-                # # Embed the iframe
-                # iframe_html = f'<iframe src="{entry.embed_url}" width="100%" height="400" frameborder="0" allowfullscreen></iframe>'
-                # st.markdown(iframe_html, unsafe_allow_html=True)
-
-        progress_bar.empty()
+                # Add a download button
+                if st.button("Download 📥"):
+                    dl_class = anipy_cli.download(entry, "best")
+                    dl_class.download()
 
 if __name__ == "__main__":
     main()
